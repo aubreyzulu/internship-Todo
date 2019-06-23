def39c7a8a1ee60b84acb7bd751b169b8ea0cf7c
Aubrey-Zulu <aubreydarious@gmail.com>
2019-06-24 00:22:14 +0200

    made a few changes

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -2,13 +2,71 @@ import React, { Component } from 'react'
 
 class TodoItems extends Component {
 
-  createTasks = item => {
-    return (
+  // createTasks = item => {
+  //   return (
      
-        <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
-          {item.text}
-        </li>
+  //       <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
+  //         {item.text}
+  //       </li>
       
+  //   )
+  // }
+  constructor() {
+    super()
+    this.state = {
+      updatedText: '',
+    }
+    this.updateText = this.updateText.bind(this);
+
+  }
+
+
+  updateText(event) {
+    this.setState({ updatedText: event.target.value })
+  }
+
+  createTasks = item => {
+
+    var txtClass = item.completed ? 'completed' : 'uncompleted';
+
+    return (
+      <li key={item.key}>
+
+        {!item.show &&
+          <div className="item-container">
+
+            <input className="status-checkbox"
+              type="checkbox" name="status"
+              value="" checked={item.completed}
+              onChange={() => this.props.changeStatus(item.key)}></input>
+
+            <span className={`item-text ${txtClass}`}
+              onClick={() => this.props.showEditItemForm(item.key)}>{item.text}</span>
+
+            <button className="delete-button"
+              onClick={() => this.props.deleteItem(item.key)}>Delete</button>
+          </div>
+        }
+        
+        {item.show &&
+          <div>
+            <input className="edit-input"
+              type="text" name="ItemText"
+              title="Item Text"
+              autoFocus={true}
+              defaultValue={item.text}
+              onFocus={this.updateText}
+              onChange={this.updateText} />
+            <button className="save"
+              onClick={() => this.props.editItem(item.key,
+                this.state.updatedText)} >Save</button>
+            <button className="cancel"
+              onClick={() => this.props.showEditItemForm(item.key)}>Close</button>
+
+          </div>
+        }
+
+      </li>
     )
   }
 
